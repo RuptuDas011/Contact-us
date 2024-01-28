428dc976aea597242b9b7b7fbb8a27ab22da7aa8
Ruptu Das <ruptudas@gmail.com>
2024-01-28 19:22:07 +0530

    first commit

---
+++ contact-us/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Navigation from './components/Navigation/Navigation'
+import Header from './components/Header/Header'
+import ContactForm from './components/ContactForm/ContactForm'
+
+function App() {
+  return (
+    <div>
+    <Navigation />
+    <main className="main_container">
+      <Header />
+      <ContactForm />
+    </main>
+  </div>
+    
+  )
+}
+
+export default App
